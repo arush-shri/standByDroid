b117b268f7e511e53da4d137681475735bdfff4e
arush-shri <arush.sdl.shrivastava@gmail.com>
2024-07-17 20:53:41 +0530

    Clock skins files created

---
+++ app/src/main/java/com/arush/standbydroid/customComponents/ClockSkins/ClockSkinFive.kt
@@ -0,0 +1,51 @@
+package com.arush.standbydroid.customComponents.clockSkins
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.arush.standbydroid.ui.theme.StandByDroidTheme
+import com.arush.standbydroid.ui.theme.provider
+
+
+@Composable
+fun ClockSkinFive(currentTime: String){
+    Column(Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement =  Arrangement.Center) {
+        Text(
+            text = currentTime,
+            style = TextStyle(
+                fontFamily = FontFamily(
+                    Font(
+                        googleFont = GoogleFont("Lobster Two"),
+                        fontProvider = provider,
+                        weight = FontWeight.Bold,
+                        style = FontStyle.Italic
+                    )
+                ),
+                fontSize = 25.sp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFive() {
+    StandByDroidTheme {
+        ClockSkinZero(currentTime = "9:11:00")
+    }
+}
+
--- app/src/main/java/com/arush/standbydroid/view/MainActivity.kt
+++ app/src/main/java/com/arush/standbydroid/view/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.arush.standbydroid.view
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import com.arush.standbydroid.customComponents.clockSkins.ClockSkinZero
 import com.arush.standbydroid.ui.theme.StandByDroidTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +31,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StandByDroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(){
+    val configuration = LocalConfiguration.current
+    val orientation = configuration.orientation
+    var currentTime by remember { mutableStateOf(getCurrentTime()) }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            while (true) {
+                delay(1000L)
+                currentTime = getCurrentTime()
+            }
+        }
+    }
+
+    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        // Landscape layout
+        LandscapeLayout(currentTime)
+    } else {
+        // Portrait layout
+        PortraitLayout(currentTime)
+    }
+}
+
+@Composable
+fun PortraitLayout(currentTime: String) {
+    Column(Modifier.fillMaxSize()) {
+        ClockSkinZero(currentTime)
+    }
+}
+
+@Composable
+fun LandscapeLayout(currentTime: String) {
+    Column(Modifier.fillMaxSize()) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StandByDroidTheme {
-        Greeting("Android")
+        HomeScreen()
     }
+}
+
+private fun getCurrentTime(): String {
+    val sdf = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+    return sdf.format(Date())
 }
