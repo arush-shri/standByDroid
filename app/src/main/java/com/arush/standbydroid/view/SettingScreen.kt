ebc89782b98f8f05c3779a951e6d61964c9ee3c5
arush-shri <arush.sdl.shrivastava@gmail.com>
2024-07-23 23:24:01 +0530

    setting screen in progress

--- app/src/main/java/com/arush/standbydroid/view/SettingScreen.kt
+++ app/src/main/java/com/arush/standbydroid/view/SettingScreen.kt
@@ -3,8 +3,16 @@ package com.arush.standbydroid.view
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,7 +20,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.sp
+import com.arush.standbydroid.R
 
 @Composable
 fun SettingScreen(){
@@ -30,30 +41,82 @@ fun SettingScreen(){
 
 @Composable
 fun LandscapeSettingLayout(orientation: Int){
-    Column(modifier = Modifier.fillMaxSize().background(color = Color.Black),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement =  Arrangement.Center,) {
-
-        Text(text = "Setting Coming Soon",
-            style = TextStyle(
-                fontSize = 28.sp,
-            ),
-            color = Color.Cyan
+    Row(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Black),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start) {
+        Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(0.2f).background(color = Color(
+            0xFF0F0F0F
         )
+        ),
+            contentAlignment = Alignment.TopStart) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                TabViewButton(buttonText = "Permission") {
+
+                }
+                TabViewButton(buttonText = "Behaviour") {
+
+                }
+                TabViewButton(buttonText = "Customize") {
+
+                }
+                TabViewButton(buttonText = "Support") {
+
+                }
+            }
+        }
+        Box(modifier = Modifier.fillMaxSize().background(color = Color(0xFF101111))) {
+            
+        }
     }
 }
 
 @Composable
 fun PortraitSettingLayout(orientation: Int){
-    Column(modifier = Modifier.fillMaxSize().background(color = Color.Black),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement =  Arrangement.Center,) {
+    val buttons = listOf("Permission", "Behaviour", "Customize", "Support")
 
-        Text(text = "Setting Coming Soon",
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Black),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(modifier = Modifier.fillMaxHeight(0.08f).fillMaxWidth().background(color = Color(0xFF0F0F0F)
+        ),
+            contentAlignment = Alignment.TopCenter) {
+            LazyRow(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                items(buttons) { buttonText ->
+                    TabViewButton(buttonText = buttonText) {
+                        // Add your onClick action here
+                    }
+                }
+            }
+        }
+        Box(modifier = Modifier.fillMaxSize().background(color = Color(0xFF101111))) {
+
+        }
+    }
+}
+
+@Composable
+fun TabViewButton(buttonText : String, callbackFunc : ()->Unit){
+    Button(
+        onClick = { callbackFunc() },
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color(0xFF575858)),
+        ) {
+        Text(
+            text = buttonText,
             style = TextStyle(
-                fontSize = 28.sp,
+                fontSize = 22.sp,
             ),
-            color = Color.Cyan
+            color = Color.White
         )
     }
 }
