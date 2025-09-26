bb6eb5378cbb424355f847b097c9aea6fb3894aa
arush-shri <arush.sdl.shrivastava@gmail.com>
2025-09-26 20:37:46 +0530

    got battery data

--- app/context/BatteryInfo.jsx
+++ app/context/BatteryInfo.jsx
@@ -4,31 +4,31 @@ const { BatteryModule } = NativeModules;
 
 const BatteryFlow = async () => {
     try {
-      const batteryInfo = await BatteryModule.getBatteryInfo();
-      const { voltage, current } = batteryInfo;
+        const batteryInfo = await BatteryModule.getBatteryInfo();
+        const { voltage, current } = batteryInfo;
 
-      if (voltage && current) {
-        // Convert units from mV and µA to V and A
-        const voltageInVolts = voltage / 1000;
-        const currentInAmperes = current / 1000000;
+        if (voltage && current) {
+            // Convert units from mV and µA to V and A
+            const voltageInVolts = voltage / 1000;
+            const currentInAmperes = current / 1000000;
 
-        // Watts = Volts * Amps
-        const wattage = voltageInVolts * currentInAmperes;
-        
-        return {wattage: wattage.toFixed(2), voltage: voltageInVolts, current: currentInAmperes};
-      } else {
-        console.log('Could not retrieve voltage or current.');
-        return null;
-      }
+            // Watts = Volts * Amps
+            const wattage = voltageInVolts * currentInAmperes;
+            
+            return {wattage: wattage.toFixed(2), voltage: voltageInVolts, current: currentInAmperes};
+        } else {
+            console.log('Could not retrieve voltage or current.');
+            return null;
+        }
     } catch (error) {
-      console.error('Error getting battery info:', error);
-      return null;
+        console.error('Error getting battery info:', error);
+        return null;
     }
 };
 
 export const GetBatteryFlow = async () => {
     const flow = await BatteryFlow();
     if (flow) {
-      return flow;
+        return flow;
     }
 };
